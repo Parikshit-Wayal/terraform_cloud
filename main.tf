82f0dc63700fee4067d9057591be9804276c2634
Parikshit Wayal <153600012+Parikshit-Wayal@users.noreply.github.com>
2025-05-17 11:55:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+main.tf
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+ 
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+  count = 3
+  tags = {
+    Name = "myinstance ${count.index+1}"
+}
+}
+ 
